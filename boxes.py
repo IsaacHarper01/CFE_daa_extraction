6a6f3c4ef41a8b9b5cada25a21e31060b835e1c5
IsaacHarper01 <121267715+IsaacHarper01@users.noreply.github.com>
2023-05-25 14:07:56 -0600

    segunda revision

--- boxes.py
+++ boxes.py
@@ -1,50 +1,65 @@
-import torch
+import argparse
+import os
+import platform
+import sys
+from pathlib import Path
 import cv2
-import numpy as np
-import pandas as pd
+import torch
+sys.path.append('/home/isaac/Isaac/Xira/CFE_data_extraction/yolov5')
 
-model = torch.hub.load('/home/isaac/Isaac/Xira/CFE_data_extraction/yolov5', 'custom', path='/home/isaac/Isaac/Xira/CFE_data_extraction/yolov5/weights/yolov5s_telmex.pt',source='local')
-aspectR_telmex_horizontal = 1.7 #this gets dividing the telmex logo width and the hight and the image is horizontal
-aspectR_telmex_vertical = 0.5625 #this gets dividing the telmex logo width and the hight and the image is vertical
-aspectR_cfe_horizontal = 2.0 
-aspectR_cfe_vertical = 0.5
-# open the image
-#image = cv2.imread('dataset2/IMG_20230418_115625.jpg')
-image = cv2.imread('dataset2/IMG_20230418_115625.jpg')
-image = cv2.resize(image,[1156,520])
-image = cv2.cvtColor(image,cv2.COLOR_BGRA2GRAY) 
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, smart_inference_mode
 
-#pass the image to the model
-detect = model(image)
-info = detect.pandas().xyxy[0]  # im1 predictions
-#info = pd.DataFrame(info)
-df = info.loc[info['name']!='barras']
-idx = df['confidence'].idxmax()
-file = df.loc[idx]
-point1 = (int(df['xmin'][1]),int(df['ymin'][1]))
-point2 = (int(df['xmax'][1]),int(df['ymax'][1]))
-print(info)
-cv2.circle(image, point1, 7, (0, 255, 0), -1)
-cv2.circle(image, point2, 7, (0, 255, 0), -1)
-file_name = df.loc[idx,'name'] 
-def orientation(df):
-    asptec_ratio = (df['xmax']-df['xmin'])/(df['ymax']-df['ymin'])
-    if df['name'] == 'cfe':
-        if 2.3 > asptec_ratio > 1.7:
-            position = 'horizontal'
-        if 0.571 > asptec_ratio > 0.425:
-            position = 'vertical'
-    else:
-        if 1.955 > asptec_ratio > 1.445:
-            position='horizontal'
-        if 0.646 > asptec_ratio > 0.477:
-            position='vertical'
-    print(df['name'],asptec_ratio)
-    return position
-    
+imgsz=(640, 640)
+path = 'val2/IMG_20230522_111213.jpg'
+image = cv2.imread(path)
+device = select_device('')
 
-print(orientation(file))
-cv2.imshow('image',image)
-#cv2.imshow('Detector de recibos', np.squeeze(detect.render()))
-cv2.waitKey(15000)
+ # Load model
+model = DetectMultiBackend('yolov5/weights/yolov5s_telmex.pt', device=device, dnn=False, data='yolov5/data/coco128.yaml', fp16=False)
+stride, names, pt = model.stride, model.names, model.pt
+imgsz = check_img_size(imgsz, s=stride) 
+print(stride, names, pt)
+#image = check_img_size(image, stride)  # check image size
+print(imgsz)
+
+dataset = LoadImages(path, img_size=imgsz, stride=stride, auto=pt, vid_stride=1)
+
+# Run inference
+model.warmup(imgsz=(1 if pt or model.triton else 1, 3, *imgsz))  # warmup
+seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+for path, im, im0s, vid_cap, s in dataset:
+    with dt[0]:
+        im = torch.from_numpy(im).to(model.device)
+        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+
+    # Inference
+    with dt[1]:
+        pred = model(im, augment=False, visualize=False)
+
+    # NMS
+    with dt[2]:
+        pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+
+    for i, det in enumerate(pred):
+        print(det)
+        im0 = im0s.copy()
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]
+        if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+                print(det)
+
+cv2.circle(image,(1821,113),5,(255,0,0),7)
+cv2.circle(image,(2318,408),5,(255,0,0),7)
+cv2.imshow('Image',image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
